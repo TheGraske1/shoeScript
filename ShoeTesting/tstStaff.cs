43e5a231f270af665e7d39bbadd6ab638c669ea1
P2428726 <P2428726@dmu.ac.uk>
2020-02-04 12:36:14 +0000

    Added testing and class for staff management.

---
+++ ShoeTesting/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoeTesting
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance
+            clsStaff AnStaff = new clsStaff();
+            //test is exists
+            Assert.IsNotNull(AnStaff);
+        }
+    }
+}
