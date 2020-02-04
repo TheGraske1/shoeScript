44b240d6143dc206e82f3a1984edbe3a7fbccece
Jay Patel <P2437771@my365.dmu.ac.uk>
2020-02-04 12:36:47 +0000

    Added testing and class for customer processing

---
+++ ShoeTesting/tstCustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoeTesting
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
