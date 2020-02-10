681fb1d5187676af248d91ac0635a71197bb5b27
Jay Patel <P2437771@my365.dmu.ac.uk>
2020-02-10 13:17:03 +0000

    added testing and class for customer processing

---
+++ ShoeTesting/tstCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoeTesting
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblCustomer]
+(
+	[CustomerID] INT NOT NULL IDENTITY , 
+    [BankID] INT NULL, 
+    [Name] VARCHAR(50) NULL, 
+    [DateCreated] DATE NULL, 
+    [Account] BIT NULL, 
+    [Balance] MONEY NULL, 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([CustomerID]) 
+)
