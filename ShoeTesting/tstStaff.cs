485f3d28fc1654138885c325cf753965f1d7ffaf
P2428726 <P2428726@dmu.ac.uk>
2020-02-11 12:56:04 +0000

    Added more testing and started web page

--- ShoeTesting/tstStaff.cs
+++ ShoeTesting/tstStaff.cs
@@ -15,5 +15,50 @@ namespace ShoeTesting
             //test is exists
             Assert.IsNotNull(AnStaff);
         }
+
+        [TestMethod]
+        public void StaffName()
+        {
+            clsStaff AnStaff = new clsStaff();
+            string testName= "John Doe";
+            AnStaff.name = testName;
+            Assert.AreEqual(AnStaff.name, testName);
+        }
+
+        [TestMethod]
+        public void StaffPNumber()
+        {
+            clsStaff AnStaff = new clsStaff();
+            string testPNum = "012345678915";
+            AnStaff.phoneNumber = testPNum;
+            Assert.AreEqual(AnStaff.phoneNumber, testPNum);
+        }
+
+        [TestMethod]
+        public void StaffJoinDate()
+        {
+            clsStaff AnStaff = new clsStaff();
+            DateTime testDate = DateTime.Now.Date;
+            AnStaff.joinedDate = testDate;
+            Assert.AreEqual(AnStaff.joinedDate, testDate);
+        }
+
+        [TestMethod]
+        public void StaffSalary()
+        {
+            clsStaff AnStaff = new clsStaff();
+            double testSalary = 25000.95;
+            AnStaff.salary = testSalary;
+            Assert.AreEqual(AnStaff.salary, testSalary);
+        }
+
+        [TestMethod]
+        public void StaffAdminPriv()
+        {
+            clsStaff AnStaff = new clsStaff();
+            Boolean testAdminPriv = true;
+            AnStaff.admin = testAdminPriv;
+            Assert.AreEqual(AnStaff.admin, testAdminPriv);
+        }
     }
 }
