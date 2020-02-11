40b38694068fd293a9cbb80c28b42531b6d5a1f0
P2422803 <grazvidas123@gmail.com>
2020-02-11 12:38:17 +0000

    Classes done + graphic started

---
+++ ClassLibrary1/clsOrderLine.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassLibrary1
+{
+    public class clsOrderLine
+    {
+        public clsOrderLine()
+        {
+        }
+
+        public int orderLineID { get; set; }
+        public int orderID { get; set; }
+        public int productID { get; set; }
+        public int quantity { get; set; }
+        public double pricePerUnit { get; set; }
+        public string selectionDescription { get; set; }
+    }
+}
