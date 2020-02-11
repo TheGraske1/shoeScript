9dc8b37c38407761f2cc159d5b0c9b4eb2fc1327
Jay Patel <P2437771@my365.dmu.ac.uk>
2020-02-11 12:09:26 +0000

    tstCustomer created

---
+++ ShoeTesting/tstCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoeTesting
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsCustomer ACustomer = new clsCustomer();
+
+            Assert.IsNotNull(ACustomer);
+        }
+    }
+}
