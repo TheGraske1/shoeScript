53d5d9b94e120adf4e9a13f792c34a7535e07472
Jay Patel <P2437771@my365.dmu.ac.uk>
2020-02-18 12:55:46 +0000

    linking the middle layer- customer

---
+++ ShoeFrontOffice/ACustomer.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary1;
+
+
+public partial class Customer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        clsCustomer ACustomer = new clsCustomer();
+
+        ACustomer = (clsCustomer)Session["ACustomer"];
+
+        Response.Write(ACustomer.name);
+        
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //clsCustomer ACustomer = new clsCustomer();
+        //ACustomer.name = txtName.Text;
+        //Session["ACustomer"] = ACustomer;
+        //Response.Redirect("CustomerViewer.aspx");
+
+    }
+}
