7a3f9c6efb0d66deb2521849c8dd53b2c19d9d94
P2428726 <P2428726@dmu.ac.uk>
2020-02-27 18:51:55 +0000

    Added Find Method

--- ShoeFrontOffice/AnStaff.aspx.cs
+++ ShoeFrontOffice/AnStaff.aspx.cs
@@ -15,9 +15,29 @@ public partial class AnStaff : System.Web.UI.Page
     }
     protected void btnOk_Click(object sender, EventArgs e)
     {
-   //     clsStaff AnStaff = new clsStaff();
- //       AnStaff.name = txtStaffName.Text;
-     //   Session["AnStaff"] = AnStaff;
-       // Response.Redirect("StaffViewer.aspx");
+        clsStaff AnStaff = new clsStaff();
+        AnStaff.name = txtStaffName.Text;
+        AnStaff.phoneNumber = txtPhoneNum.Text;
+        AnStaff.salary = Convert.ToDouble(txtSalary.Text);
+        AnStaff.joinedDate = Convert.ToDateTime(txtJoinedDate.Text);
+        AnStaff.admin = chckAdmin.Checked;
+        Session["AnStaff"] = AnStaff;
+        Response.Write("StaffViewer.aspx");
+    }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        clsStaff AnStaff = new clsStaff();
+        int StaffId;
+        Boolean Found = false;
+        StaffId = Convert.ToInt32(txtStaffId.Text);
+        Found = AnStaff.Find(StaffId);
+        if (Found == true)
+        {
+            txtStaffName.Text = AnStaff.name;
+            txtPhoneNum.Text = AnStaff.phoneNumber;
+            txtSalary.Text = AnStaff.salary.ToString();
+            txtJoinedDate.Text = AnStaff.joinedDate.ToString();
+        }
     }
 }
