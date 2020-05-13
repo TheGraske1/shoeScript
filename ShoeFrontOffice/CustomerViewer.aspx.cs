c3b65c87841fd5f4f2008b88c3da18f86b629b32
Jay <jaypatel928@yahoo.co.uk>
2020-05-13 18:22:51 +0100

    Finished.

---
+++ ShoeFrontOffice/CustomerViewer.aspx.cs
@@ -0,0 +1,17 @@
+﻿using ShoeClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CustomerViewer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        clsCustomer ACustomer = new clsCustomer();
+        ACustomer = (clsCustomer)Session["ACustomer"];
+        Response.Write(ACustomer.Name);
+    }
+}
