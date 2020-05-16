55aa586be69d24abe4a1e025fb1d1e3a3324f39a
Vashishth <vashishth008.vk@gmail.com>
2020-05-16 15:50:57 +0100

    clsStockCollection and tstStockCollection added

---
+++ ClassLibrary1/clsStockCollection.cs
@@ -0,0 +1,56 @@
+﻿using ShoeClasses;
+using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public class clsStockCollection
+    {
+        List<clsStock> mProductList = new List<clsStock>();
+
+        public clsStockCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_ProductTable_SelectAll");
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+                clsStock AProduct = new clsStock();
+                AProduct.LimitedEdition = Convert.ToBoolean(DB.DataTable.Rows[Index]["LimitedEdition"]);
+                AProduct.ProductId = Convert.ToInt32(DB.DataTable.Rows[Index]["ProductId"]);
+                AProduct.StyleName = Convert.ToString(DB.DataTable.Rows[Index]["StyleName"]);
+                AProduct.QuantityAvailable = Convert.ToInt32(DB.DataTable.Rows[Index]["QuantityAvailable"]);
+                AProduct.BackInStockDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["BackInStockDate"]);
+                AProduct.Price = Convert.ToDouble(DB.DataTable.Rows[Index]["Price"]);
+                mProductList.Add(AProduct);
+                Index++;
+            }
+        }
+
+        public List<clsStock> ProductList
+        {
+            get
+            {
+                return mProductList;
+            }
+            set
+            {
+                mProductList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mProductList.Count;
+            }
+            set
+            {
+                
+            }
+        }
+        public clsStock ThisProduct { get; set; }
+    }
+}
---
+++ ShoeTesting/tstStockCollection.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ShoeTesting
+{
+    [TestClass]
+    public class tstStockCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStockCollection AllProducts = new clsStockCollection();
+            Assert.IsNotNull(AllProducts);
+        }
+
+        [TestMethod]
+        public void ProductListOK()
+        {
+            clsStockCollection AllProducts = new clsStockCollection();
+            List<clsStock> TestList = new List<clsStock>();
+            clsStock TestItem = new clsStock();
+            TestItem.ProductId = 1;
+            TestItem.StyleName = "Test Style name";
+            TestItem.BackInStockDate = DateTime.Now.Date;
+            TestItem.Price = 9.99;
+            TestItem.QuantityAvailable = 100;
+            TestItem.LimitedEdition = true;
+            TestList.Add(TestItem);
+            AllProducts.ProductList = TestList;
+            Assert.AreEqual(AllProducts.ProductList, TestList);
+        }
+
+       
+
+        [TestMethod]
+        public void ThisProductPropartyOK()
+        {
+            clsStockCollection AllProducts = new clsStockCollection();
+            clsStock TestProduct = new clsStock();
+            TestProduct.ProductId = 1;
+            TestProduct.StyleName = "Test Style name";
+            TestProduct.BackInStockDate = DateTime.Now.Date;
+            TestProduct.Price = 9.99;
+            TestProduct.QuantityAvailable = 100;
+            TestProduct.LimitedEdition = true;
+            AllProducts.ThisProduct = TestProduct;
+            Assert.AreEqual(AllProducts.ThisProduct, TestProduct);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsStockCollection AllProducts = new clsStockCollection();
+            List<clsStock> TestList = new List<clsStock>();
+            clsStock TestItem = new clsStock();
+            TestItem.ProductId = 1;
+            TestItem.StyleName = "Test Style name";
+            TestItem.BackInStockDate = DateTime.Now.Date;
+            TestItem.Price = 9.99;
+            TestItem.QuantityAvailable = 100;
+            TestItem.LimitedEdition = true;
+            TestList.Add(TestItem);
+            AllProducts.ProductList = TestList;
+            Assert.AreEqual(AllProducts.Count, TestList.Count);
+
+        }
+
+        /* [TestMethod]
+        public void CountPropartyOK()
+        {
+            clsStockCollection ALLProducts = new clsStockCollection();
+            Int32 SomeCount = 2;
+            ALLProducts.Count = SomeCount;
+            Assert.AreEqual(ALLProducts.Count, SomeCount);
+        }
+        [TestMethod]
+        public void TwoRecordsPresent()
+        {
+            clsStockCollection AllProducts = new clsStockCollection();
+            Assert.AreEqual(AllProducts.Count, 2);
+        }*/
+    }
+}
